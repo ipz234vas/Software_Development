fe532dffe6fc8aa8d5d8c44c48e8efbbf08847de
Волинець Андрій Сергійович <ipz234_vas@student.ztu.edu.ua>
2025-03-14 11:37:44 +0200

    task3: implement Singleton

---
+++ lab-2/Singleton/Authenticator.cs
@@ -0,0 +1,55 @@
+﻿namespace Singleton
+{
+    using System;
+
+    public sealed class Authenticator
+    {
+        private static Authenticator? _instance;
+        private static object _lock = new object();
+        private string? _currentLogin;
+        private bool _isAuthenticated;
+
+        public bool IsAuthenticated => _isAuthenticated;
+        public string? Login => _currentLogin;
+
+        private Authenticator()
+        {
+            Console.WriteLine("Initialize Authenticator");
+        }
+
+        public static Authenticator GetInstance()
+        {
+            if (_instance is null)
+            {
+                lock (_lock)
+                {
+                    if (_instance is null)
+                    {
+                        _instance = new Authenticator();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public bool Authenticate(string login, string password)
+        {
+            // hardcoded authentication check example
+            if (login != "admin" || password != "password")
+            {
+                return false;
+            }
+            _currentLogin = login;
+            _isAuthenticated = true;
+            return true;
+        }
+
+        public bool Logout()
+        {
+            _currentLogin = null;
+            _isAuthenticated = false;
+            return true;
+        }
+    }
+
+}
---
+++ lab-2/Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using Singleton;
+
+Task task1 = Task.Run(() =>
+{
+    var auth1 = Authenticator.GetInstance();
+    if (auth1.Authenticate("admin", "password"))
+        Console.WriteLine("Task 1: Authenticated");
+});
+
+Task task2 = Task.Run(() =>
+{
+    var auth2 = Authenticator.GetInstance();
+    Thread.Sleep(100);
+    if (auth2.IsAuthenticated)
+        Console.WriteLine("Task 2: Already authenticated as " + auth2.Login);
+});
+
+Task.WaitAll(task1, task2);
+
+var authenticator1 = Authenticator.GetInstance();
+var authenticator2 = Authenticator.GetInstance();
+
+if (authenticator1 == authenticator2)
+    Console.WriteLine("Main: Authenticator instance is the same.");
+
+if (authenticator1.Logout())
+    Console.WriteLine("Main: Successfully logged out.");
