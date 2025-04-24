a041e3e970937135eb614ac6efb9f5c863f56112
Волинець Андрій Сергійович <ipz234_vas@student.ztu.edu.ua>
2025-04-24 11:44:49 +0300

    init mediator project

---
+++ lab-4/Mediator/CommandCentre.cs
@@ -0,0 +1,16 @@
+﻿using Mediator;
+
+namespace Mediator
+{
+    class CommandCentre
+    {
+        private List<Runway> _runways = new List<Runway>();
+        private List<Aircraft> _aircrafts = new List<Aircraft>();
+
+        public CommandCentre(Runway[] runways, Aircraft[] aircrafts)
+        {
+            this._runways.AddRange(runways);
+            this._aircrafts.AddRange(aircrafts);
+        }
+    }
+}
---
+++ lab-4/Mediator/Runway.cs
@@ -0,0 +1,30 @@
+﻿using Mediator;
+
+namespace Mediator
+{
+    class Runway
+    {
+        public readonly Guid Id = Guid.NewGuid();
+        public Aircraft? IsBusyWithAircraft;
+
+        public bool CheckIsActive()
+        {
+            bool result = false;
+            if (this.IsBusyWithAircraft != null)
+            {
+                result = this.IsBusyWithAircraft.IsTakingOff;
+            }
+            return result;
+        }
+
+        public void HighLightRed()
+        {
+            Console.WriteLine($"Runway {this.Id} is busy!");
+        }
+
+        public void HighLightGreen()
+        {
+            Console.WriteLine($"Runway {this.Id} is free!");
+        }
+    }
+}
